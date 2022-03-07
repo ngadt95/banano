bdac3e9e385b470c54710cc38437e667ff6f3b54
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-03-07 16:52:36 +0700

    Update banano.sh

--- banano.sh
+++ banano.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 wget -O fahclient.deb https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb
 sudo dpkg -i --force-depends fahclient.deb
+
+
+
+
+
 cd /etc/fahclient/
 sudo FAHClient --configure
 cat config.xml
